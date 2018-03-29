9847cfd67e50f0fce2f2fd41aef943425f8dffaf
arkadiusz salek <salek.arek@gmail.com>
2018-03-29 23:57:24 +0100

    changes for gallery.html

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,13 @@
+$(document).ready(function(){
+  $(".column").css("display",'none');
+  $(".column").slideDown(1500);
+});
+
+$(document).ready(function(){
+  $(".landing-text").css("display",'none');
+  $(".landing-text").slideDown(1500);
+});
+
 const menuButton = document.getElementById("menu-button");
 const mobileNav = document.querySelector(".mobile-nav");
 
@@ -7,3 +17,39 @@ function openMenu() {
   mobileNav.classList.toggle("is-open");
   this.classList.toggle("square");
 }
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+  document.getElementById('navbar').style.display = "none";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+  document.getElementById('navbar').style.display = "block";
+
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+

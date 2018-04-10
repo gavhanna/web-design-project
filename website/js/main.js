6094ac15dc634cf3c05d07146aaa6b5dc05eaa60
Gav Hanna <gavhanna@gmail.com>
2018-04-10 19:48:02 +0100

    re-added mobile nav toggle javascript

--- website/js/main.js
+++ website/js/main.js
@@ -1,10 +1,20 @@
-$(document).ready(function(){
-  $(".landing-text").css("display",'none');
+const menuButton = document.getElementById("menu-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+
+$(document).ready(function () {
+  $(".landing-text").css("display", 'none');
   $(".landing-text").slideDown(1500);
-});
-$(document).ready(function(){
-  $(".column").css("display",'none');
+  $(".column").css("display", 'none');
   $(".column").slideDown(1500);
+
+  menuButton.addEventListener("click", openMenu);
+
+  function openMenu() {
+    mobileNav.classList.toggle("is-open");
+    this.classList.toggle("square");
+  }
+
 });
 
 
@@ -34,15 +44,19 @@ function showSlides(n) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }

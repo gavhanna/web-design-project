80b8be88e09519a2babe1656ba89625c1acf6e0c
Gav Hanna <gavhanna@gmail.com>
2018-04-14 16:54:42 +0100

    made contact page responsive and added small animations

--- website/js/main.js
+++ website/js/main.js
@@ -15,11 +15,12 @@ $(document).ready(function () {
   $(".column").css("display", 'none');
   $(".column").fadeIn(2000);
 
-  reserve-container
+  // reserve - container
 
   menuButton.addEventListener("click", openMenu);
 
   function openMenu() {
+    console.log("OPEN");
     mobileNav.classList.toggle("is-open");
     this.classList.toggle("square");
   }
@@ -76,38 +77,45 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
-function go(){
+function go() {
   var x = document.forms["myform"]["fname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;}
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
   var y = document.forms["myform"]["people"].value;
-    if (y == "") {
-        alert("Number of guests field must be filled out");
-        return false;}
+  if (y == "") {
+    alert("Number of guests field must be filled out");
+    return false;
+  }
   var z = document.forms["myform"]["date"].value;
-    if (z == "") {
-        alert("Date must be filled out");
-        return false;}
+  if (z == "") {
+    alert("Date must be filled out");
+    return false;
+  }
   var h = document.forms["myform"]["time"].value;
-    if (h == "") {
-          alert("Time must be filled out");
-          return false;}
+  if (h == "") {
+    alert("Time must be filled out");
+    return false;
+  }
   var i = document.forms["myform"]["email"].value;
-    if (i == "") {
-          alert("Email must be filled out");
-          return false;}
+  if (i == "") {
+    alert("Email must be filled out");
+    return false;
+  }
 
   alert('We will contact you within an hour');
 }
